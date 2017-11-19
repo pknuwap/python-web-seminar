fb11a0dffd6b70fdbe841123b8f30182121d725e
hallazzang <hallazzang@gmail.com>
2017-11-19 15:33:46 +0900

    NoteApp 작업 중, 스타일 덜 적용됨

--- noteapp/app.py
+++ noteapp/app.py
@@ -1,3 +1,5 @@
+from functools import wraps
+
 import flask
 from werkzeug.security import generate_password_hash, check_password_hash
 
@@ -22,6 +24,16 @@ class NewNoteForm(FlaskForm):
     to = StringField('받는 사람', validators=[DataRequired()])
     title = StringField('제목', validators=[DataRequired()])
     content = TextAreaField('내용', validators=[DataRequired()])
+
+def login_required(view):
+    @wraps(view)
+    def decorated_view(*args, **kwargs):
+        if 'user' not in flask.session:
+            flask.flash('로그인을 해주세요.', 'error')
+            return flask.redirect(flask.url_for('login'))
+
+        return view(*args, **kwargs)
+    return decorated_view
 
 app = flask.Flask(__name__)
 app.config['SECRET_KEY'] = 'kkdffkdgfdgkdfkg'
@@ -96,23 +108,24 @@ def logout():
     return flask.redirect(flask.url_for('index'))
 
 @app.route('/note/new', methods=['GET', 'POST'])
+@login_required
 def write_note():
-    if 'user' not in flask.session:
-        flask.flash('로그인을 해주세요.', 'error')
-        return flask.redirect(flask.url_for('login'))
-
     new_note_form = NewNoteForm()
     if flask.request.method == 'POST' and new_note_form.validate():
         to = new_note_form.to.data
         title = new_note_form.title.data
         content = new_note_form.content.data
 
-        if mongo.db.users.count({'id': to}) == 0:
+        user = mongo.db.users.find_one({'id': to})
+        if not user:
             flask.flash('존재하지 않는 사용자입니다.', 'error')
             return flask.redirect(flask.url_for('write_note'))
 
         mongo.db.notes.insert_one({
-            'to': to,
+            'to': {
+                'id': to,
+                'name': user['name'],
+            },
             'sender': {
                 'id': flask.session['user']['id'],
                 'name': flask.session['user']['name'],
@@ -122,16 +135,30 @@ def write_note():
         })
 
         flask.flash('쪽지를 보냈습니다.', 'success')
-        return flask.redirect(flask.url_for('note_list'))
+        return flask.redirect(flask.url_for('note_list', box='outbox'))
 
     return flask.render_template('new_note.html', new_note_form=new_note_form)
 
-@app.route('/note/list')
-def note_list():
-    if 'user' not in flask.session:
-        flask.flash('로그인을 해주세요.', 'error')
-        return flask.redirect(flask.url_for('login'))
+@app.route('/note/<box>')
+@login_required
+def note_list(box):
+    if box == 'inbox':
+        notes = mongo.db.notes.find({'to.id': flask.session['user']['id']})
+    elif box == 'outbox':
+        notes = mongo.db.notes.find({'sender.id': flask.session['user']['id']})
+    else:
+        flask.abort(404)
 
-    notes = mongo.db.notes.find({'to': flask.session['user']['id']})
+    return flask.render_template('note_list.html', box=box, notes=notes)
 
-    return flask.render_template('note_list.html', notes=notes)
+@app.route('/note/content/<ObjectId:id>')
+@login_required
+def note_content(id):
+    note = mongo.db.notes.find_one_or_404({'_id': id})
+
+    if (flask.session['user']['id'] != note['to']['id'] and
+        flask.session['user']['id'] != note['sender']['id']):
+        flask.flash('자신의 쪽지만 볼 수 있습니다.', 'error')
+        return flask.redirect(flask.url_for('note_list', box='inbox'))
+
+    return flask.render_template('note_content.html', note=note)
